6af4229907159b7f7f4b42bc1b55ef3fb101a442
WvrLight <sSLight22@gmail.com>
2022-07-17 14:26:51 +0800

    terstttt

--- src/index.php
+++ src/index.php
@@ -8,7 +8,13 @@
     while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
     // This will loop through each row, now use your loop here
         print $row["custusername"];
+        print "\n-------\n";
+        print $row["custUsername"];
+        print "\n-------\n";
         print $row["id"];
+        print "\n-------\n";
         print $row["custfullname"];
+        print "\n-------\n";
+        print $row["custFullName"];
     }
 ?>
