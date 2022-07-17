5ac4c304091c1c8c0a001e317f559183b785aeba
WvrLight <sSLight22@gmail.com>
2022-07-17 23:00:54 +0800

    login fix 2

--- src/login.php
+++ src/login.php
@@ -28,37 +28,34 @@
             </ul>
         </div>
 		<br><br><br><br><br><br><br><br>
+        <form method="post" name="login-form">
 			<fieldset class="login-form">
-			<form method="post" name="login-form">
-					<div>
-						<h1 align="center"> Login Form </h1>
-							<i class="fa fa-user icon"></i>
-								<input type="text" class="username-login" name="username" placeholder="Enter Username" required><br><br>
-							<i class="fa fa-key icon"></i>
-								<input type="password" class="pass-login" name="password" placeholder="Enter Password" required><br><br>
-							<p><a href="signup.html" class="signuplink">Don't have an account?</a> 
-							<br>
-							<div align="right">
-								<input type='submit' class="loginbtn" name='submitBtn' value='Login' />
-								<input type='reset' class="clearbtn" name='resetBtn' value='Reset'/>
-							</div>
-					</div>	
-			</form>
+                <div>
+                    <h1 align="center"> Login Form </h1>
+                        <i class="fa fa-user icon"></i>
+                            <input type="text" class="username-login" name="username" placeholder="Enter Username" required><br><br>
+                        <i class="fa fa-key icon"></i>
+                            <input type="password" class="pass-login" name="password" placeholder="Enter Password" required><br><br>
+                        <p><a href="signup.html" class="signuplink">Don't have an account?</a> 
+                        <br>
+                        <div align="right">
+                            <input type='submit' class="loginbtn" name='submitBtn' value='Login' />
+                            <input type='reset' class="clearbtn" name='resetBtn' value='Reset'/>
+                        </div>
+                </div>
 			</fieldset>
-			
+        </form>
         <div id="footer">
             Copyright &copy; 2022 <a href="index.html">Filipino Meal Kits.</a> Rights Reserved. <br>
             <a href="mailto:fmk@filipinomealkits.com">fmk@filipinomealkits.com</a>
             <p> <a href="termsandconditions.html">Terms and Conditions.</a>&nbsp&nbsp&nbsp&nbsp<a href="privacypolicy.html">Privacy Policy.</a></p>
-            
         </div>
-		
     </body>
 </html>
 
 <?php
-    	if (isset($_POST['username'], $_POST['password'])) {
-		$sql = "SELECT * FROM Customer WHERE custUsername = '" . $_REQUEST['username'] . "' AND custPassword = '" . $_REQUEST['password'] . "'";
+    if (isset($_POST['username'], $_POST['password'])) {
+		$sql = "SELECT * FROM Customer WHERE custUsername = '" . $_POST['username'] . "' AND custPassword = '" . $_POST['password'] . "'";
 		$stmt = $pdo->prepare($sqluser);
 		$stmt->execute();
 		$check = stmt->rowCount();
@@ -69,7 +66,7 @@
 		if ($check == 1)
 		{
 			$_SESSION['id'] = $check['id'];
-			$_SESSION['username']= $_REQUEST['username'];
+			$_SESSION['username']= $_POST['username'];
             $_SESSION['cart'] = array();
 			echo "<script>alert('Login successful!');</script>";
 			echo "<script>window.location.href='inventory.php'</script>";
