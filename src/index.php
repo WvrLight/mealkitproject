73c3a8a092ea6b43e236b3ba5dff05a060e40dc4
WvrLight <sSLight22@gmail.com>
2022-07-17 14:12:06 +0800

    test

--- src/index.php
+++ src/index.php
@@ -5,8 +5,19 @@
     $stmt = $pdo->prepare($sql);
     $stmt->execute();
 
+    echo "a";
+    print "b";
     while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
     // This will loop through each row, now use your loop here
-        print $row['custUsername'];
+        print $row["custUsername"];
     }
+
+    $sql2 = 'SELECT * FROM Customer';
+    $stmt2 = $pdo->prepare($sql);
+    $stmt2->execute();
+    $details = $stmt2->fetch();
+
+    echo "c";
+    print "d";
+    print_r($details);
 ?>
