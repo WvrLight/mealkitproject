a5df21d17c28577d58c220682a603f8ee779cfc2
WvrLight <sSLight22@gmail.com>
2022-07-17 14:15:45 +0800

    works now?

--- src/index.php
+++ src/index.php
@@ -7,22 +7,7 @@
 
     echo "a";
     print "b";
-    while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-    // This will loop through each row, now use your loop here
-        print $row["custusername"];
-        print $row["custUsername"];
-    }
     foreach($details as $test) {
         print $test["custusername"];
-        print $test["custUsername"];
     }
-
-    $sql2 = 'SELECT * FROM Customer';
-    $stmt2 = $pdo->prepare($sql);
-    $stmt2->execute();
-    $details = $stmt2->fetch(PDO::FETCH_OBJ);
-
-    echo "c";
-    print "d";
-    print_r($details->custusername);
 ?>
