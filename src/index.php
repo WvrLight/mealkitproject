fe4b6578d57b8bafc136f101056825566bd52dcb
WvrLight <sSLight22@gmail.com>
2022-07-17 14:06:47 +0800

    1

--- src/index.php
+++ src/index.php
@@ -4,11 +4,9 @@
     $sql = 'SELECT * FROM Customer';
     $stmt = $pdo->prepare($sql);
     $stmt->execute();
-    $rowCount = $stmt->rowCount();
-    $details = $stmt->fetch();
 
     while ($row = $stmt->fetch(PDO::FETCH_OBJ)) {
     // This will loop through each row, now use your loop here
-        print("<p>" . $row->custusername . "</p>\n");
+        print $row->custUsername;
     }
 ?>
