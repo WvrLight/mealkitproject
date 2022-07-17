679c399ae9ee1980aba7c42b7692735523858dd5
WvrLight <sSLight22@gmail.com>
2022-07-17 14:14:49 +0800

    test2

--- src/index.php
+++ src/index.php
@@ -12,13 +12,17 @@
         print $row["custusername"];
         print $row["custUsername"];
     }
+    foreach($details as $test) {
+        print $test["custusername"];
+        print $test["custUsername"];
+    }
 
     $sql2 = 'SELECT * FROM Customer';
     $stmt2 = $pdo->prepare($sql);
     $stmt2->execute();
-    $details = $stmt2->fetch(PDO::FETCH_ASSOC);
+    $details = $stmt2->fetch(PDO::FETCH_OBJ);
 
     echo "c";
     print "d";
-    print_r($details);
+    print_r($details->custusername);
 ?>
