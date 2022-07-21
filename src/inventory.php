9af73882bb7abdc4b4675f7e348ef494b10e11b5
WvrLight <sSLight22@gmail.com>
2022-07-21 22:15:18 +0800

    product view button fix

--- src/inventory.php
+++ src/inventory.php
@@ -67,11 +67,11 @@
 									}
 									echo "</div>";
 									echo "<div class='view'>
-									<input type='submit' class='button' name='view' value='View'>
+									<input type='submit' onclick=\"location.href='#formView'\" class='button' name='view' value='View'>
 									<input type='submit' class='button' name='addtocart' value='Add to Cart'>";
 									if (isset($_SESSION['isadmin'])) {
-										echo "<input type='submit' onclick='location.href='#formEdit'' class='button' name='edit' value='Edit'>
-										<input type='submit' onclick='location.href='#formView'' class='button' name='remove' value='Remove'>";
+										echo "<input type='submit' onclick=\"location.href='#formEdit'\" class='button' name='edit' value='Edit'>
+										<input type='submit' class='button' name='remove' value='Remove'>";
 									}
 								echo "</div>";
 							echo "</fieldset>
