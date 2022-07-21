31117ef4f2429d3861c74ebae5c4836b1a5ed23c
WvrLight <sSLight22@gmail.com>
2022-07-22 04:30:25 +0800

    print

--- src/inventoryview.php
+++ src/inventoryview.php
@@ -1,49 +1,5 @@
 <?php
     $id = $_GET['id'];
 
-    $sql = "SELECT * FROM Product WHERE id = " . $id;
-    $stmt = $pdo->prepare($sql);
-    $stmt->execute();
-
-    while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-        if ($row['productsaleprice'] == null) {
-            echo "
-            <div class='popupView'>
-                <a class='close' href='#'>×</a>
-                <div class='viewProduct'>
-                    <img id='viewProductUrl' src='" . $row['productimgurl'] . "' class='viewPic'/>
-                    <div class='viewDesc'>
-                        <h2 id='viewProductName'>" . $row['productname'] ."</h2>
-                        <h5 id='viewProductSaleDate'>Sale until " . $row['productsaleend'] . "</h5>
-                        <div class='prices'>
-                            <p id='viewProductPrice' class='price'>Php " . $row['productprice'] . "</p>
-                        </div>
-                        <h3> Description: </h3>
-                        <p id='viewProductDesc' class='paraTxt'>" . $row['productdesc'] ."</p>
-                    </div>
-                </div>
-            </div>
-            ";
-        }
-        else {
-            echo "
-            <div class='popupView'>
-                <a class='close' href='#'>×</a>
-                <div class='viewProduct'>
-                    <img id='viewProductUrl' src='" . $row['productimgurl'] . "' class='viewPic'/>
-                    <div class='viewDesc'>
-                        <h2 id='viewProductName'>" . $row['productname'] ."</h2>
-                        <h5 id='viewProductSaleDate'>Sale until " . $row['productsaleend'] . "</h5>
-                        <div class='prices'>
-                            <p id='viewProductOldPrice'class='old-price'>Php " . $row['productprice'] . "</p>
-                            <p id='viewProductPrice' class='price'>Php " . $row['productsaleprice'] . "</p>
-                        </div>
-                        <h3> Description: </h3>
-                        <p id='viewProductDesc' class='paraTxt'>" . $row['productdesc'] ."</p>
-                    </div>
-                </div>
-            </div>
-            ";
-        }
-    }
+    echo $id;
 ?>
