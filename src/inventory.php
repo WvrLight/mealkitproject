efe04e95fc1e5b97cedc088343ba6b7ae13bc47a
WvrLight <sSLight22@gmail.com>
2022-07-22 03:46:38 +0800

    move function

--- src/inventory.php
+++ src/inventory.php
@@ -1,6 +1,35 @@
 <?php include ('db.php')?>
 <?php 
 	session_start();
+
+	function view($id) {
+		echo "<script>console.log('a')</script>";
+		echo "<script>console.log('" . $id . "')</script>";
+		$sql = "SELECT * FROM Product WHERE productId = " . $id;
+		$stmt = $pdo->prepare($sql);
+		$stmt->execute();
+
+		while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+			echo "<script>document.getElementById('viewProductName').innerHTML = '" . $row['productname'] . "'</script>";
+			echo "<script>document.getElementById('viewProductDesc').innerHTML = '" . $row['productdesc'] . "'</script>";
+			echo "<script>document.getElementById('viewProductUrl').innerHTML = '" . $row['productimgurl'] . "'</script>";
+			echo "<script>document.getElementById('viewProductSaleDate').innerHTML = '" . $row['productsaleend'] . "'</script>";
+
+			if ($row['productsaleprice'] == null) {
+				echo "<script>document.getElementById('viewProductOldPrice').innerHTML = ''</script>";
+				echo "<script>document.getElementById('viewProductPrice').innerHTML = '" . $row['productprice'] . "'</script>";
+			}
+			else {
+				echo "<script>document.getElementById('viewProductOldPrice').innerHTML = '" . $row['productprice'] . "'</script>";
+				echo "<script>document.getElementById('viewProductPrice').innerHTML = '" . $row['productsaleprice'] . "'</script>";
+			}
+		}
+
+		echo "<script>
+			var view = document.getElementById('formView');
+			view.focus();
+		</script>";
+	}
 ?>
 
 <!DOCTYPE html>
@@ -175,33 +204,4 @@
 			count.innerText = '" . count($_SESSION['cart']) . "';
 		</script>");
 	};
-
-	function view($id) {
-		echo "<script>console.log('a')</script>";
-		echo "<script>console.log('" . $id . "')</script>";
-		$sql = "SELECT * FROM Product WHERE productId = " . $id;
-		$stmt = $pdo->prepare($sql);
-		$stmt->execute();
-
-		while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-			echo "<script>document.getElementById('viewProductName').innerHTML = '" . $row['productname'] . "'</script>";
-			echo "<script>document.getElementById('viewProductDesc').innerHTML = '" . $row['productdesc'] . "'</script>";
-			echo "<script>document.getElementById('viewProductUrl').innerHTML = '" . $row['productimgurl'] . "'</script>";
-			echo "<script>document.getElementById('viewProductSaleDate').innerHTML = '" . $row['productsaleend'] . "'</script>";
-
-			if ($row['productsaleprice'] == null) {
-				echo "<script>document.getElementById('viewProductOldPrice').innerHTML = ''</script>";
-				echo "<script>document.getElementById('viewProductPrice').innerHTML = '" . $row['productprice'] . "'</script>";
-			}
-			else {
-				echo "<script>document.getElementById('viewProductOldPrice').innerHTML = '" . $row['productprice'] . "'</script>";
-				echo "<script>document.getElementById('viewProductPrice').innerHTML = '" . $row['productsaleprice'] . "'</script>";
-			}
-		}
-
-		echo "<script>
-			var view = document.getElementById('formView');
-			view.focus();
-		</script>";
-	}
 ?>
