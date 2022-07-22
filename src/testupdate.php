2889e9cfe8b5e6aa5bcf77b622c6781dfdd2d0b7
WvrLight <sSLight22@gmail.com>
2022-07-22 15:41:18 +0800

    tablename

--- src/testupdate.php
+++ src/testupdate.php
@@ -1,7 +1,7 @@
 <?php include ('db.php')?>
 
 <?php
-    $sql = "INSERT INTO Products(productName, productImgUrl, productPrice)
+    $sql = "INSERT INTO Product(productName, productImgUrl, productPrice)
             VALUES ('test', 'assets\img\adobo.jpg', 150)";
     $stmt = $pdo->prepare($sql);
     $stmt->execute();
