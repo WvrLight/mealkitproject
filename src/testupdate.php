9deb40be1c5a2203cea8ac875d23946cd784c51e
WvrLight <sSLight22@gmail.com>
2022-07-22 15:38:42 +0800

    TEST

--- src/testupdate.php
+++ src/testupdate.php
@@ -1,6 +1,6 @@
 <?php
     $sql = "UPDATE Product
-            SET productName = 'test'l
+            SET productName = 'test'
             WHERE id = 3";
     $stmt = $pdo->prepare($sql);
     $stmt->execute();
