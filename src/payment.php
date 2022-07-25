2b53f3a91879caf2e84558b91d3940ff9ec27343
WvrLight <sSLight22@gmail.com>
2022-07-25 23:21:21 +0800

    test

--- src/payment.php
+++ src/payment.php
@@ -73,7 +73,9 @@
         }
 
         var cod = document.getElementById("radio_cod");
-        cod.onclick => {
+        cod.onclick = setToCOD;
+        
+        setToCOD() {
              document.getElementById("card_holder").required = false;
              document.getElementById("card_holder").innerText = "";
              document.getElementById("card_holder").setAttribute('disabled', '');
@@ -92,7 +94,9 @@
         }
 
         var card = document.getElementById("radio_card");
-        card.onclick => {
+        card.onclick = setToCard;
+        
+        setToCard() {
              document.getElementById("card_holder").required = true;
              document.getElementById("card_holder").removeAttribute('disabled');
 
